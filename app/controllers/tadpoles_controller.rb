18da7632070e6f4101d46739971c3a9879ad4fc9
Justin Daniel <justinddaniel@gmail.com>
2017-11-28 03:24:56 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -4,7 +4,7 @@ class TadpolesController < ApplicationController
   def metamorphosize
     @frog = Frog.create(:name => @tadpole.name, :color => @tadpole.color, :pond => @tadpole.pond)
     @tadpole.destroy
-    redirect_to frogs_path(@frog)
+    redirect_to ("/frogs/#{@frog.id}")
   end
 
   def index
